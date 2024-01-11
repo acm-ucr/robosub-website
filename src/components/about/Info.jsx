e7ca014ac5eba1b171002120a1a729e1717e9d0f
veritasia <aminaapstudent@yahoo.com>
2024-01-11 15:18:59 -0800

    updated info.jsx

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,19 +1,16 @@
 const format =
-  "rounded-xl border-robosub-yellow border-[1px] flex flex-col items-center  w-[31%] py-2 pb-4 border-opacity-70 text-transparent bg-clip-text bg-gradient-to-r from-white to-slate-500";
+  "rounded-xl border-robosub-yellow border-[1px] flex flex-col items-center  w-[31%] py-2 pb-4 border-opacity-70 text-transparent bg-clip-text bg-gradient-to-r from-white to-robosub-dark-gray";
 const bigNumberFormat = "text-[90px] font-bold leading-none";
 
 const Info = () => {
   return (
     <div className=" w-1/2">
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center mb-1">
         <p class="text-robosub-yellow font-bold text-base">Year founded</p>
-        <p class="font-bold text-transparent bg-clip-text bg-gradient-to-r from-white to-slate-500 text-[90px] leading-none">
+        <p class="font-bold text-transparent bg-clip-text bg-gradient-to-r from-white to-robosub-dark-gray text-[90px] leading-none">
           2015
         </p>
       </div>
-
-      <br />
-      <br />
 
       <div className="flex justify-between">
         <div className={format}>
