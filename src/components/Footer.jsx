0d8d8bd7a3ca42ee2cee14f5002cb318063fa8ee
Shing Hung <123037099+ysh0305@users.noreply.github.com>
2024-01-10 23:13:27 -0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,48 +6,43 @@ import { links } from "../data/footerLinks";
 
 const Footer = () => {
   return (
-    <div>
-      <div className="w-full relative">
-        <Image alt="img1" src={img1} className="w-full" />
+    <div className="w-full px-10 bg-white h-[33vh] flex justify-between font-sans relative">
+      <Image
+        alt="img1"
+        src={img1}
+        className="w-screen absolute left-0 bottom-1/3 -z-10"
+      />
+      <Link
+        href="/"
+        className="flex w-1/4 items-center hover:opacity-60 duration-300"
+      >
+        <Image alt="logo" src={logo} className=" object-contain" />
+        <div className="text-robosub-dark-gray text-3xl ml-2">
+          <p className="font-medium">ROBOSUB</p>
+          <p>UCR</p>
+        </div>
+      </Link>
+      <div className="flex flex-col justify-center items-center text-robosub-black text-xl">
+        <p className="font-bold">University of California, Riverside</p>
+        <p className="font-bold">Autonomous Underwater Vechicle</p>
+        <p>Winston Chung Hall, Riverside, CA</p>
       </div>
-      <div className="w-full bg-white h-96">
-        <div className="absolute flex flex-col-2 ml-14 mt-40 ">
-          <Image
-            alt="logo"
-            src={logo}
-            className="h-max hover:opacity-75 duration-300"
-          />
-          <div>
-            <p className="font-sans font-medium text-robosub-dark-gray text-3xl ">
-              ROBOSUB
-            </p>
-            <p className="font-sans text-robosub-dark-gray text-3xl">UCR</p>
-          </div>
+      <div className="flex items-center text-robosub-black">
+        <div className="text-xl flex flex-col items-end">
+          <p>Contact Us</p>
+          <p className="font-bold">robosubucr@gmail.com</p>
         </div>
-        <div className="flex justify-center items-center translate-y-40 text-robosub-black text-xl">
-          <div>
-            <p className="font-bold">University of California, Riverside</p>
-            <p className="font-bold">Autonomous Underwater Vechicle</p>
-            <p>Winston Chung Hall, Riverside, CA</p>
-          </div>
-        </div>
-        <div className="flex justify-end items-center text-robosub-black">
-          <div className="font-sans text-xl flex flex-col items-end">
-            <p>Contact Us</p>
-            <p className="font-bold">robosubucr@gmail.com</p>
-          </div>
-          <div className="text-6xl border-l-black border-l-2 pl-6 ml-7 m-10">
-            {links.map((item, index) => (
-              <Link
-                href={item.link}
-                target="_blank"
-                key={index}
-                className="hover:text-robosub-yellow hover:scale-90 duration-300"
-              >
-                {item.icon}
-              </Link>
-            ))}
-          </div>
+        <div className="text-5xl border-l-black border-l-2 pl-6 ml-7">
+          {links.map((item, index) => (
+            <Link
+              href={item.link}
+              target="_blank"
+              key={index}
+              className="hover:text-robosub-yellow hover:text-[45px] duration-300"
+            >
+              {item.icon}
+            </Link>
+          ))}
         </div>
       </div>
     </div>
