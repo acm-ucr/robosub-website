358c809d0c22f910577f6e3dd51a163209cf394e
veritasia <aminaapstudent@yahoo.com>
2024-01-11 14:09:43 -0800

    Implemented Info.jsx without border gradident

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,7 +1,36 @@
+const format =
+  "rounded-xl border-robosub-yellow border-[1px] flex flex-col items-center  w-[31%] py-2 pb-4 border-opacity-70 text-transparent bg-clip-text bg-gradient-to-r from-white to-slate-500";
+const bigNumberFormat = "text-[90px] font-bold leading-none";
+
 const Info = () => {
   return (
-    <div className="">
-      <p>Info</p>
+    <div className=" w-1/2">
+      <div className="flex flex-col items-center">
+        <p class="text-robosub-yellow font-bold text-base">Year founded</p>
+        <p class="font-bold text-transparent bg-clip-text bg-gradient-to-r from-white to-slate-500 text-[90px] leading-none">
+          2015
+        </p>
+      </div>
+
+      <br />
+      <br />
+
+      <div className="flex justify-between">
+        <div className={format}>
+          <p class={bigNumberFormat}>4</p>
+          <p class="font-bold text-lg">Vehicles Designed</p>
+        </div>
+
+        <div className={format}>
+          <p class={bigNumberFormat}>6</p>
+          <p class="font-bold text-lg">Different Majors</p>
+        </div>
+
+        <div className={format}>
+          <p class={bigNumberFormat}>4</p>
+          <p class="font-bold text-lg text-center">Competitions Involved</p>
+        </div>
+      </div>
     </div>
   );
 };
