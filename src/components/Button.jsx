4f760b52ee8e194e4c43d788c8fa7ee4bc437b3f
Shing Hung <yhung022@ucr.edu>
2024-01-10 16:26:58 -0800

    fix button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Button = ({ link, text }) => {
   return (
     <Link
-      className="border-robosub-light-gray border-[1px] rounded-lg py-2 px-8 font-light text-robosub-light-gray transition-opacity ease-in-out hover:opacity-60 active:opacity-40"
+      className=" w-fit border-robosub-light-gray border-[1px] rounded-lg py-2 px-8 font-light text-robosub-light-gray transition-opacity ease-in-out hover:opacity-60 active:opacity-40"
       href={link}
     >
       {text}
