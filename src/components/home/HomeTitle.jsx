e571295bef064480d437e72fd8caeefbcaf2b247
Shing Hung <yhung022@ucr.edu>
2024-01-10 23:40:31 -0800

    fix footer

--- src/components/home/HomeTitle.jsx
+++ src/components/home/HomeTitle.jsx
@@ -6,19 +6,19 @@ import { SlArrowDown } from "react-icons/sl";
 
 const HomeTitle = () => {
   return (
-    <div className="w-full relative -translate-y-5">
+    <div className="w-full relative -translate-y-5 -z-10">
       <Image src={image1} alt="Background image 1" className="w-screen" />
       <Image
         src={image2}
         alt="Background image 2"
-        className="w-screen top-1/2 absolute z-10"
+        className="w-screen top-1/2 absolute"
       />
-      <div className="w-full right-[7%] bottom-[30%] absolute flex flex-col items-end z-10 text-xl">
+      <div className="w-full right-[7%] bottom-[30%] absolute flex flex-col items-end text-xl">
         <p className="italic font-thin">Presenting</p>
         <p className="italic font-bold">Laviathan 2024</p>
         <SlArrowDown className="m-3 mr-8 text-2xl" />
       </div>
-      <div className="text-[80px] w-full left-[15%] absolute top-[28%] z-10">
+      <div className="text-[80px] w-full left-[15%] absolute top-[28%]">
         <p className="drop-shadow-lg shadow-black font-bold text-robosub-yellow translate-y-6 relative">
           UCR
         </p>
