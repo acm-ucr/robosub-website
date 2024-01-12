b2caaadfe061643cc67a9c7aa3114b2072d0b2c1
Armando Zepeda <armandozepeda@MacBookPro.local>
2024-01-12 04:51:00 -0800

    Support

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Button = ({ link, text }) => {
   return (
     <Link
-      className=" w-fit border-robosub-light-gray border-[1px] rounded-lg py-2 px-8 font-light text-robosub-light-gray transition-opacity ease-in-out hover:opacity-60 active:opacity-40"
+      className="  text-xs w-fit border-robosub-light-gray border-[1px] rounded-lg font-light text-robosub-light-gray transition-opacity ease-in-out hover:opacity-60 active:opacity-40 py-1 px-4   "
       href={link}
     >
       {text}
