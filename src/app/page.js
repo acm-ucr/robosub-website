ee5c55741c1825f320c47ebbe2dc3bd28aba1c92
eche2004 <139306587+eche2004@users.noreply.github.com>
2024-01-13 14:13:26 -0800

    description of your changes

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,20 @@ import Image from "next/image";
 import JoinIcons from "@/components/home/JoinIcons";
 import imagebar from "../public/imageBar.png";
 import InfoBox from "@/components/home/InfoBox";
+import JoinTeam from "@/components/home/JoinTeam";
+import { BsBank } from "react-icons/bs";
+
 const Home = () => {
   return (
     <div className="mb-[30%] w-full flex flex-col items-center min-h-screen">
       <HomeTitle />
       <InfoBox />
       <Subtitle title="Join the Experience" text="Open to all UCR students!" />
+      <JoinTeam
+        title="Busniess & Analytics"
+        text="Network with potential sponsors, recruit new members, and build the foundation for the team."
+        icon={<BsBank />}
+      />
       <Image src={imagebar} alt="image bar" />
       <Subtitle title="Links" text="Get Involved" />
       <JoinIcons />
--- src/components/home/JoinTeam.jsx
+++ src/components/home/JoinTeam.jsx
@@ -1,7 +1,51 @@
-const JoinTeam = () => {
+import React from "react";
+import Button from "../Button";
+
+const JoinTeam = ({ icon, title, text }) => {
   return (
-    <div className="">
-      <p>JoinTeam</p>
+    <div className=" flex bg-robosub-black w-[30%] items-start">
+      <div className="bg-black-500 font-sans text-8xl aspect-square w-96 rounded-full flex justify-center items-center bg-gradient-to-tr from-robosub-black to-robosub-dark-gray">
+        {icon}
+      </div>
+
+      <div
+        className=" flex flex-col items-center text-1xl font-sans"
+        style={{ paddingTop: "20px" }}
+      >
+        <p
+          style={{
+            fontWeight: "600",
+            fontSize: "23px",
+            marginBottom: "10px",
+            marginLeft: "-10px",
+          }}
+        >
+          {title}
+        </p>
+
+        <p
+          style={{
+            fontWeight: "145",
+            fontSize: "15px",
+            marginLeft: "20px",
+            marginTop: "8px",
+          }}
+        >
+          {text}
+        </p>
+        <div
+          style={{
+            marginLeft: "auto",
+            width: "100%",
+            display: "flex",
+            justifyContent: "flex-end",
+            marginBottom: "30px",
+            marginTop: "0px",
+          }}
+        >
+          <Button link="/join" text="TEAM" />
+        </div>
+      </div>
     </div>
   );
 };
