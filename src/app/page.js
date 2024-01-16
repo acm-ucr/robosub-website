1a92648ae60af1d855cf304e7e0c7a21e5f0a811
Shing Hung <yhung022@ucr.edu>
2024-01-16 11:13:19 -0800

    fix JoinTeam

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,6 @@ import Image from "next/image";
 import JoinIcons from "@/components/home/JoinIcons";
 import imagebar from "../public/imageBar.png";
 import InfoBox from "@/components/home/InfoBox";
-import JoinTeam from "@/components/home/JoinTeam";
-import { BsBank } from "react-icons/bs";
 
 const Home = () => {
   return (
@@ -14,11 +12,6 @@ const Home = () => {
       <HomeTitle />
       <InfoBox />
       <Subtitle title="Join the Experience" text="Open to all UCR students!" />
-      <JoinTeam
-        title="Busniess & Analytics"
-        text="Network with potential sponsors, recruit new members, and build the foundation for the team."
-        icon={<BsBank />}
-      />
       <Image src={imagebar} alt="image bar" />
       <Subtitle title="Links" text="Get Involved" />
       <JoinIcons />
--- src/components/home/JoinTeam.jsx
+++ src/components/home/JoinTeam.jsx
@@ -3,46 +3,16 @@ import Button from "../Button";
 
 const JoinTeam = ({ icon, title, text }) => {
   return (
-    <div className=" flex bg-robosub-black w-[30%] items-start">
-      <div className="bg-black-500 font-sans text-8xl aspect-square w-96 rounded-full flex justify-center items-center bg-gradient-to-tr from-robosub-black to-robosub-dark-gray">
+    <div className="bg-robosub-black w-[400px] items-center flex">
+      <div className="font-sans text-[84px] pb-2 aspect-square w-[45%] rounded-full flex justify-center items-center bg-gradient-to-br to-black from-robosub-dark-gray">
         {icon}
       </div>
 
-      <div
-        className=" flex flex-col items-center text-1xl font-sans"
-        style={{ paddingTop: "20px" }}
-      >
-        <p
-          style={{
-            fontWeight: "600",
-            fontSize: "23px",
-            marginBottom: "10px",
-            marginLeft: "-10px",
-          }}
-        >
-          {title}
-        </p>
+      <div className=" flex flex-col items-center leading-4 font-sans w-3/4 pl-6 pt-4">
+        <p className="font-bold text-xl">{title}</p>
 
-        <p
-          style={{
-            fontWeight: "145",
-            fontSize: "15px",
-            marginLeft: "20px",
-            marginTop: "8px",
-          }}
-        >
-          {text}
-        </p>
-        <div
-          style={{
-            marginLeft: "auto",
-            width: "100%",
-            display: "flex",
-            justifyContent: "flex-end",
-            marginBottom: "30px",
-            marginTop: "0px",
-          }}
-        >
+        <p className="pt-4 pb-2 font-light leading-4 text-xs">{text}</p>
+        <div className="w-full flex justify-end">
           <Button link="/join" text="TEAM" />
         </div>
       </div>
