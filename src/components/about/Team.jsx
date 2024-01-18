54f69de870c90e8a3ffcafe3a09a6530fb29578d
Shing Hung <yhung022@ucr.edu>
2024-01-17 20:16:43 -0800

    fix team

--- src/components/about/Team.jsx
+++ src/components/about/Team.jsx
@@ -1,12 +1,12 @@
 const Team = ({ icon, title, text, list }) => {
   return (
-    <div className="w-7/12 flex flex-col justify-items-start font-sans text-robosub-light-gray space-y-2">
-      <div className=" text-[80px] font-bold bg-clip-text">{icon}</div>
-      <p className="text-4xl text-robosub-yellow font-semibold">{title}</p>
-      <p className="text-lg leading-relaxed">{text}</p>
+    <div className="w-7/12 flex flex-col justify-items-start font-sans text-robosub-light-gray space-y-4">
+      <div className=" text-6xl font-bold bg-clip-text">{icon}</div>
+      <p className="text-3xl text-robosub-yellow font-semibold">{title}</p>
+      <p className=" leading-5 font-light">{text}</p>
       <div className="font-semibold tracking-tight">
         {list.map((item, index) => (
-          <div className="flex items-center" key={index}>
+          <div className="flex items-center text-sm" key={index}>
             <div className="ml-3 w-1 h-1 mr-1 rounded-full bg-white" />
             {item}
           </div>
