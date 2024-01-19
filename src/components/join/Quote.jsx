02f2d619d457f026a15d2f799f1357c0e1e575a6
Motunrayooluwa Ogunwusi <motunrayooluwaogunwusi@MotunraluwasAir.junior.emma.troy.ny.us>
2024-01-18 20:59:51 -0500

    I created a quote

--- src/components/join/Quote.jsx
+++ src/components/join/Quote.jsx
@@ -1,7 +1,9 @@
-const Quote = () => {
+const Quote = ({ quote, person }) => {
   return (
-    <div className="">
-      <p>Quote</p>
+    <div className=" border-robosub-yellow bg-clip-text bg-gradient-to-tr from-robosub-yellow to-robosub-dark-gray rounded-xl items-center pt-2 pb-4 w-1/3 duration-200 border-[1px] border-opacity-70 font-light py-1 px-7 ">
+      <p></p>
+      <p className="font-thin"> {quote} </p>
+      <p className="pt-1 font-bold text-sm"> {person} </p>
     </div>
   );
 };
