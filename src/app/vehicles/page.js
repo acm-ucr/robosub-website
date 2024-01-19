a9a2c48d56388c00a44da0fddc68998483e89337
Armando Zepeda <armandozepeda@ucr-secure-39-10-13-204-63.wnet.ucr.edu>
2024-01-19 10:49:17 -0800

    created leviathan video

--- src/app/vehicles/page.js
+++ src/app/vehicles/page.js
@@ -1,7 +1,11 @@
+import Leviathan from "@/components/vehicle/Leviathan";
+
 const Vehicles = () => {
   return (
     <div className="mb-[30%] w-full flex flex-col items-center min-h-screen">
       <p>Vehicles</p>
+
+      <Leviathan />
     </div>
   );
 };
--- src/components/vehicle/Leviathan.jsx
+++ src/components/vehicle/Leviathan.jsx
@@ -1,7 +1,20 @@
 const Leviathan = () => {
   return (
-    <div className="">
-      <p>Leviathan</p>
+    <div className=" w-1/2 flex flex-col items-center font-sans text-sm">
+      <p className=" font-bold my-5 !text-6xl text-robosub-yellow ">
+        LEVIATHAN
+      </p>
+
+      <p>
+        Leviathan is our 2021 autonomous underwater vehicle (AUV) designed to be
+        &quot;future-proof&quot; for the next five years. It features new
+        competitive abilities including ease of motion, passive stability,
+        bi-directional vision, and designated locations for future improvements.
+      </p>
+
+      <p className="w-full mb-10 mt-2">
+        Check out our report and the updates below!
+      </p>
     </div>
   );
 };
