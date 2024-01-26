8814e40c6df8b1c71b7285d4dbbcd9402d7f7cc8
Shing Hung <yhung022@ucr.edu>
2024-01-25 21:26:57 -0800

    remove bootstrap

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,53 +4,45 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { items } from "@/data/navigation";
 import Image from "next/image";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
 
 const NavBar = () => {
   const [selected, setSelected] = useState("");
   return (
-    <Navbar
-      collapseOnSelect
-      expand="md"
-      fixed="top"
-      className="absolute top-0 z-30 h-24 bg-robosub-black opacity-70 w-full flex justify-between items-center backdrop-blur-sm"
-    >
-      <Navbar.Brand className="p-0 hover:opacity-60 duration-300">
-        <Link className="p-0 flex items-center ml-4" href="/">
-          <Image alt="logo" src={logo} />
-        </Link>
-      </Navbar.Brand>
-      <Navbar.Collapse className="items-center md:justify-end justify-center flex">
-        <Nav className="mb-2 mr-16 flex text-xl items-center">
-          {items.map((item, index) => (
-            <Nav.Link
-              href={item.link}
-              key={index}
-              onClick={() => {
-                setSelected(item.name);
-              }}
-              className={`mb-0  px-4 whitespace-nowrap mr-20 hover:text-robosub-yellow duration-300 ${
-                selected === item.name && "underline"
-              }`}
-            >
-              {item.name}
-            </Nav.Link>
-          ))}
-          <Nav.Link
-            href="/join"
+    <div className="md:px-7 fixed z-30 h-8 md:h-[10vh] bg-robosub-black bg-opacity-60 w-screen flex md:justify-between items-center backdrop-blur text-[10px] md:text-lg pl-2">
+      <Link
+        onClick={() => {
+          setSelected("");
+        }}
+        className="hover:opacity-60 duration-300 w-1/5"
+        href="/"
+      >
+        <Image alt="logo" src={logo} />
+      </Link>
+      <div className="md:pl-0 pl-3 flex items-center md:w-1/2 justify-between w-3/4">
+        {items.map((item, index) => (
+          <Link
+            href={item.link}
+            key={index}
             onClick={() => {
-              setSelected("JOIN");
+              setSelected(item.name);
             }}
-            className={`border-blue-950 border-[1px] bg-blue-950 rounded-lg hover:opacity-60 duration-300  py-2 px-7 ${
-              selected === "JOIN" ? "scale-125" : "scale-100"
+            className={`hover:text-robosub-yellow duration-300 border-dashed ${
+              selected === item.name
+                ? "border-b-2 border-robosub-yellow text-robosub-yellow"
+                : "text-robosub-light-gray"
             }`}
           >
-            JOIN
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+            {item.name}
+          </Link>
+        ))}
+        <Link
+          href="https://discord.com/invite/gjFMdGwqUw"
+          className={` bg-robosub-blue rounded-lg hover:opacity-60 duration-300 py-1 px-4 md:px-6`}
+        >
+          JOIN
+        </Link>
+      </div>
+    </div>
   );
 };
 export default NavBar;
