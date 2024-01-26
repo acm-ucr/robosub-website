f2e00b249fa617734a8a6e71c20e2af56ff3ab43
veritasia <aminaapstudent@yahoo.com>
2024-01-25 20:24:31 -0800

    Improved mobile responsiveness for Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,15 +6,15 @@ import { links } from "../data/footerLinks";
 
 const Footer = () => {
   return (
-    <div className="w-full px-10 bg-white h-[33vh] flex justify-between font-sans relative">
+    <div className="w-full px-10 bg-white md:h-[33vh] md:flex md:my-0 my-10 justify-between font-sans relative">
       <Image
         alt="img1"
         src={img1}
-        className="w-screen absolute left-0 bottom-1/3 -z-10"
+        className="md:w-screen absolute md:left-0 bottom-1/3 -z-10"
       />
       <Link
         href="/"
-        className="flex w-[28%] items-center hover:opacity-60 duration-300"
+        className="flex w-[28%] items-center hover:opacity-60 duration-300 right-10"
       >
         <Image alt="logo" src={logo} className=" object-contain" />
         <div className="text-robosub-dark-gray text-3xl ml-2">
@@ -22,27 +22,29 @@ const Footer = () => {
           <p>UCR</p>
         </div>
       </Link>
-      <div className="flex flex-col justify-center items-center text-robosub-black text-xl">
+      <div className="flex flex-col justify-center items-center text-robosub-black text-xl my-5 md:my-0">
         <p className="font-bold">University of California, Riverside</p>
         <p className="font-bold">Autonomous Underwater Vechicle</p>
         <p>Winston Chung Hall, Riverside, CA</p>
       </div>
-      <div className="flex items-center text-robosub-black">
-        <div className="text-xl flex flex-col items-end">
-          <p>Contact Us</p>
-          <p className="font-bold">robosubucr@gmail.com</p>
-        </div>
-        <div className="text-5xl border-l-black border-l-2 pl-6 ml-7">
-          {links.map((item, index) => (
-            <Link
-              href={item.link}
-              target="_blank"
-              key={index}
-              className="hover:text-robosub-yellow hover:text-[45px] duration-300"
-            >
-              {item.icon}
-            </Link>
-          ))}
+      <div className="flex justify-end">
+        <div className="flex items-center text-robosub-black">
+          <div className="text-xl flex flex-col items-end">
+            <p>Contact Us</p>
+            <p className="font-bold">robosubucr@gmail.com</p>
+          </div>
+          <div className="text-5xl border-l-black border-l-2 pl-6 ml-7 mb-5 md:mb-0 flex flex-col items-end">
+            {links.map((item, index) => (
+              <Link
+                href={item.link}
+                target="_blank"
+                key={index}
+                className="hover:text-robosub-yellow hover:text-[45px] duration-300"
+              >
+                {item.icon}
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </div>
