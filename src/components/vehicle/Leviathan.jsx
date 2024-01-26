d44315354da590ec9cdba0d3325fc5af388be0e6
Shing Hung <yhung022@ucr.edu>
2024-01-25 21:52:33 -0800

    fix size

--- src/components/vehicle/Leviathan.jsx
+++ src/components/vehicle/Leviathan.jsx
@@ -1,11 +1,11 @@
 const Leviathan = () => {
   return (
-    <div className=" md:w-1/2  flex flex-col items-center font-sans md:text-sm text-[8px]  px-11 md:px-0 ">
+    <div className=" md:w-1/2 flex flex-col items-center font-sans md:text-sm text-[10px] px-11 md:px-0 ">
       <p className=" font-bold md:my-5 my-2 md:!text-6xl text-3xl text-robosub-yellow  ">
         LEVIATHAN
       </p>
 
-      <p className="  ">
+      <p>
         Leviathan is our 2021 autonomous underwater vehicle (AUV) designed to be
         &quot;future-proof&quot; for the next five years. It features new
         competitive abilities including ease of motion, passive stability,
