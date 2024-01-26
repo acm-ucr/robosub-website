7f1343dc75e53a100182070a8917813c867a0616
Armando Zepeda <armandozepeda@MacBookPro.local>
2024-01-25 21:18:18 -0800

    phone format

--- src/components/home/JoinIcons.jsx
+++ src/components/home/JoinIcons.jsx
@@ -2,7 +2,7 @@ import { links } from "@/data/links";
 import JoinIcon from "./JoinIcon";
 const JoinIcons = () => {
   return (
-    <div className="flex w-[45%] justify-between my-10">
+    <div className="md:flex md:w-[45%] justify-between my-8 md:pe-[0%] pe-[60%]  ">
       {links.map((item, index) => (
         <JoinIcon
           key={index}
--- src/components/vehicle/Leviathan.jsx
+++ src/components/vehicle/Leviathan.jsx
@@ -1,11 +1,11 @@
 const Leviathan = () => {
   return (
-    <div className=" w-1/2 flex flex-col items-center font-sans text-sm">
-      <p className=" font-bold my-5 !text-6xl text-robosub-yellow ">
+    <div className=" md:w-1/2  flex flex-col items-center font-sans md:text-sm text-[8px]  px-11 md:px-0 ">
+      <p className=" font-bold md:my-5 my-2 md:!text-6xl text-3xl text-robosub-yellow  ">
         LEVIATHAN
       </p>
 
-      <p>
+      <p className="  ">
         Leviathan is our 2021 autonomous underwater vehicle (AUV) designed to be
         &quot;future-proof&quot; for the next five years. It features new
         competitive abilities including ease of motion, passive stability,
