9c2ec2d896bad2596450dc4b7186d79d3a4ed922
Sweden Agunenye <swedenagu@Swedens-MacBook-Air.local>
2024-01-31 23:18:25 -0800

    fixed join component mobile responsiveness format

--- src/components/join/Quotes.jsx
+++ src/components/join/Quotes.jsx
@@ -2,7 +2,7 @@ import { quotes, quotes2 } from "@/data/quotes";
 import Quote from "./Quote";
 const Quotes = () => {
   return (
-    <div className="grid grid-cols-2 mt-[3%] w-2/3">
+    <div className="grid md:grid-cols-2 grid-cols-1 mt-[3%] md:p-0 p-11 md:w-2/3 w-max-full">
       <div>
         {quotes.map((item, index) => (
           <Quote
