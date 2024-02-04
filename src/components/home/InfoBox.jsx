0e9307524e93a683033d286be23d3ce21886665d
Armando Zepeda <armandozepeda@MacBookPro.local>
2024-02-03 19:23:46 -0800

    2xl format

--- src/components/home/InfoBox.jsx
+++ src/components/home/InfoBox.jsx
@@ -3,18 +3,20 @@ import image1 from "@/public/project.webp";
 import image2 from "@/public/vehicle1.webp";
 import Button from "../../components/Button";
 import { AiOutlineDoubleRight } from "react-icons/ai";
-const format = "font-sans font-light md:text-[23px] text-[13px]";
+const format = "font-sans font-light md:text-[23px] text-[13px] 2xl:text-3xl";
 
 const InfoBox = () => {
   return (
     <div className="flex flex-col items-center w-[70%]">
-      <div className="flex flex-col md:flex-row md:justify-between md:mb-[7%] mb-[50%]">
+      <div className="flex flex-col md:flex-row md:justify-between md:mb-[7%] mb-[50%] 2xl:w-[90%]">
         <div className="flex w-full mb-6 md:mb-0 md:w-[55%] flex-col ">
           <div className="flex items-center mb-6 md:mb-[12%]">
             <AiOutlineDoubleRight className=" text-robosub-blue text-2xl md:text-4xl" />
-            <p className="font-bold text-xl md:text-[35px]">Our Project</p>
+            <p className="font-bold text-xl md:text-[35px] 2xl:text-5xl ">
+              Our Project
+            </p>
           </div>
-          <div className="mb-6 md:mb-[10%]">
+          <div className="mb-6 md:mb-[10%] ">
             <p className={format}>
               We are University of California, Riverside&apos;s
             </p>
@@ -26,12 +28,12 @@ const InfoBox = () => {
           </div>
           <Button link="/about" text="LEARN MORE" />
         </div>
-        <div className="object-contain w-full md:w-[45%]">
+        <div className="object-contain w-full md:w-[45%]   ">
           <Image src={image1} alt="Project Image" />
         </div>
       </div>
 
-      <div className="flex flex-col md:flex-row md:justify-between">
+      <div className="flex flex-col md:flex-row md:justify-between 2xl:w-[90%]">
         <div className="object-contain w-full mb-6 md:w-[45%]">
           <Image src={image2} alt="Vehicle Image" />
         </div>
@@ -39,9 +41,11 @@ const InfoBox = () => {
         <div className="flex flex-col items-end w-full md:w-[55%]">
           <div className="flex items-center mb-6 md:mb-[12%]">
             <AiOutlineDoubleRight className=" text-robosub-blue text-2xl md:text-4xl" />
-            <p className="font-bold text-xl md:text-[35px]">Our Vehicle</p>
+            <p className="font-bold text-xl md:text-[35px 2xl:text-5xl">
+              Our Vehicle
+            </p>
           </div>
-          <div className="flex flex-col items-end mb-6 md:mb-[10%]">
+          <div className="flex flex-col items-end mb-6 md:mb-[10%] 2xl:text-4xl">
             <p className={format}>
               Leviathan features five degrees of motion, and is
             </p>
