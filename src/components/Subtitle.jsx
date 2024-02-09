5b188b01c74e9bff7da7688af34c7aaf67c69ba0
Shing Hung <yhung022@ucr.edu>
2024-02-08 22:11:05 -0800

    fix text size

--- src/components/Subtitle.jsx
+++ src/components/Subtitle.jsx
@@ -1,11 +1,11 @@
 const Subtitle = ({ title, text }) => {
   return (
     <div className="w-full pl-[15%] md:mb-8 mt-24">
-      <p className="flex flex-col text-robosub-yellow font-bold text-2xl md:text-4xl 2xl:text-8xl">
+      <p className="text-robosub-yellow font-bold text-2xl md:text-4xl 2xl:text-5xl">
         {" "}
         {title}{" "}
       </p>
-      <p className="italic font-thin text-xs md:text-base 2xl:text-5xl">
+      <p className="italic font-thin text-xs md:text-base 2xl:text-xl">
         {" "}
         {text}{" "}
       </p>
--- src/components/home/Support.jsx
+++ src/components/home/Support.jsx
@@ -2,13 +2,13 @@ import Button from "../Button";
 const Support = () => {
   return (
     <div className=" flex flex-col items-center w-full mt-28">
-      <p className=" text-2xl md:text-4xl 2xl:text-6xl mb-4 2xl:mb-6 text-center text-white font-semibold">
+      <p className=" text-2xl md:text-4xl 2xl:text-5xl mb-4 2xl:mb-6 text-center text-white font-semibold">
         SUPPORT UCR ROBOSUB!
       </p>
-      <p className="max-w-[90%] text-center text-sm md:text-xl 2xl:text-3xl mb-2 2xl:mb-4">
+      <p className="max-w-[90%] text-center text-sm md:text-xl 2xl:text-2xl mb-2 2xl:mb-4">
         WE RELY ON YOUR HELP TO KEEP OUR TEAM RUNNING AND FREE FOR
       </p>
-      <p className="text-sm md:text-xl 2xl:text-3xl mb-4 2xl:mb-6">
+      <p className="text-sm md:text-xl 2xl:text-2xl mb-4 2xl:mb-6">
         ALL UCR STUDENTS
       </p>
 
