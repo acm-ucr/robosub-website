8bb976bf3738b8419b362162ae7a40b9e9b3075d
Shing Hung <yhung022@ucr.edu>
2024-02-16 11:30:11 -0800

    Map Videos

--- src/app/vehicles/page.js
+++ src/app/vehicles/page.js
@@ -1,12 +1,14 @@
 import PageTitle from "@/components/PageTitle";
 import vehicle from "@/public/vehicle.webp";
 import Leviathan from "@/components/vehicle/Leviathan";
+import Videos from "@/components/vehicle/Videos";
 
 const Vehicles = () => {
   return (
     <div className="mb-[30%] w-full flex flex-col items-center min-h-screen">
       <PageTitle image={vehicle} title="Vehicles" text="See Our AUV" />
       <Leviathan />
+      <Videos />
     </div>
   );
 };
---
+++ src/data/videos.js
@@ -0,0 +1,18 @@
+export const videos = [
+  {
+    title: "Water Test - 5/30/23",
+    link: "https://www.youtube.com/embed/SfYQFnJBnpo",
+  },
+  {
+    title: "Water Test (Buoyancy) - 5/30/23",
+    link: "https://www.youtube.com/embed/mlAjlZN7h68",
+  },
+  {
+    title: "Sensor Optimization: GUI & Computer Vision",
+    link: "https://www.youtube.com/embed/v0zuGl1FZXI",
+  },
+  {
+    title: "Hull Design: Thruster Layout",
+    link: "https://www.youtube.com/embed/l9_8VFHU2Xs",
+  },
+];
