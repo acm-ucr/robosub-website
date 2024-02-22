98a2c5c8c0ad1fdb9c285495016edd092ab0843d
veritasia <aminaapstudent@yahoo.com>
2024-02-21 22:34:02 -0800

    Added animation to Info.jsx

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -3,16 +3,16 @@ const format =
 const bigNumberFormat =
   "md:text-8xl 2xl:text-9xl text-5xl font-bold leading-none";
 const borderformat =
-  "rounded-xl bg-gradient-to-b from-robosub-orange via-robosub-light-yellow to-robosub-orange p-[1px] md:w-30 w-[32%]";
+  "rounded-xl bg-gradient-to-b from-robosub-orange via-robosub-light-yellow to-robosub-orange p-[1px] md:w-30 w-[32%] animate-fade-down animate-delay-[600ms]";
 
 const Info = () => {
   return (
     <div className="md:w-1/2 w-[80%]">
       <div className="flex flex-col items-center mb-9 md:mb-11">
-        <p className="text-robosub-yellow font-bold text-sm md:text-base 2xl:text-xl">
+        <p className="text-robosub-yellow font-bold text-sm md:text-base 2xl:text-xl animate-fade-down animate-delay-200">
           Year founded
         </p>
-        <p className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-white to-robosub-dark-gray text-5xl 2xl:text-8xl md:text-[80px] leading-none">
+        <p className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-white to-robosub-dark-gray text-5xl 2xl:text-8xl md:text-[80px] leading-none animate-fade-down animate-delay-200">
           2015
         </p>
       </div>
