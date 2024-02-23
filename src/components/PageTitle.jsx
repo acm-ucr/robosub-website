a60de27ab79b45e7af0bd05a93e749a1f9d41940
eche2004 <139306587+eche2004@users.noreply.github.com>
2024-02-23 10:29:34 -0800

    added animation for the pagetitle

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 import image2 from "@/public/blue-wave.svg";
 const PageTitle = ({ image, title, text }) => {
   return (
-    <div className="w-full relative -z-10 -mb-10">
+    <div className="w-full relative -z-10 -mb-10 animate-fade-right animate-once animate-delay-100">
       <Image src={image} alt="Background image 1" className="w-screen" />
       <Image
         src={image2}
         alt="Background image 2"
         className="w-screen top-[43%] absolute"
       />
-      <div className="flex flex-col left-[15%] absolute top-[30%] md:top-[40%] 2xl:text-6xl md:text-5xl text-xl font-sans font-bold">
+      <div className="flex flex-col left-[15%] absolute top-[30%] md:top-[40%] 2xl:text-6xl md:text-5xl text-xl font-sans font-bold ">
         <p className="text-robosub-yellow">{title}</p>
         <p>{text}</p>
       </div>
