4bb50ee4acc9ef919d114f7309e41715701b032e
dylanwong6605 <dylanwong6605@gmail.com>
2024-02-23 15:01:55 -0800

    added Questions and Quotes animation

--- src/components/join/Quotes.jsx
+++ src/components/join/Quotes.jsx
@@ -10,6 +10,7 @@ const Quotes = () => {
             key={index}
             quote={item.quote}
             person={item.person}
+            animation={item.animation}
           />
         ))}
       </div>
@@ -20,6 +21,7 @@ const Quotes = () => {
             key={index}
             quote={item.quote}
             person={item.person}
+            animation={item.animation}
           />
         ))}
       </div>
